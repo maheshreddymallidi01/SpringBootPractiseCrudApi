6f59ec4401c589aa7555fc85070f4e941b1e72b9
Mahesh <maheshreddymallidi01@gmail.com>
2022-11-03 23:45:12 +0530

    CrudApi

---
+++ src/main/java/com/springbootpractise/crudapi/SpringBootPractiseCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpractise.crudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractiseCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractiseCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootpractise/crudapi/SpringBootPractiseCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootpractise.crudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPractiseCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
